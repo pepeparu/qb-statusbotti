cd41a4e2a2e0d3bea08eb419bf11dd4258c68eba
El Pepe <76623780+pepeparu@users.noreply.github.com>
2022-01-27 20:06:58 +0200

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,12 @@
+Config = {}
+
+
+---Viesti
+Config.Webhookki = "https://discord.com/api/webhooks/923596716618440756/2PFD7FuPQBmW1A0ayrMhCAg3dVPrTaIN8lylMHcMIfA9agz-zizzcHw9ws0LFhKaqh7E" --Laita webhookki tähän
+Config.Viesti = "935957240735727636" 
+----Embedi
+Config.Vari = 3447003 -- Värejä löydät mm. täältä https://gist.github.com/thomasbnt/b6f455e2c7d743b796917fa3c205f812
+Config.Avatari = "https://cdn.discordapp.com/attachments/773923782641319947/935570425767927868/nordicci_4.png"
+
+
+Config.Paivitysaika = 7 -- Kuinka monen sekunnin välein päivittyy
